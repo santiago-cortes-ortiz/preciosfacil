1f4bb4ca78009adc7c69c79a8129c2c889740673
jeisson <jeissoncortes02@outlook.com>
2025-08-07 22:43:51 -0500

    set variables

--- perciosfacil/settings.py
+++ perciosfacil/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f)1k3db17w+mqy!$pa7@%s+5fj=xc7*c*i!p+1ko(y#hrt7!9^'
+SECRET_KEY = getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
