c2dae038d66cf18783b2e505ae4a8d5b106074ce
jeisson <jeissoncortes02@outlook.com>
2025-08-09 15:10:14 -0500

    add secret

--- perciosfacil/settings.py
+++ perciosfacil/settings.py
@@ -12,10 +12,13 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from os import getenv
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+"""Carga variables de entorno desde .env en el directorio BASE_DIR"""
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
