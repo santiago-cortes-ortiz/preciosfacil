2ef73bf9f91bef733ff582481ba93bd398b9fc99
jeisson <jeissoncortes02@outlook.com>
2025-08-09 16:35:17 -0500

    change mod debug

--- perciosfacil/settings.py
+++ perciosfacil/settings.py
@@ -27,7 +27,7 @@ load_dotenv(BASE_DIR / '.env')
 SECRET_KEY = getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
